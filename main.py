8b3bbd886eb0b8f22dfdf8f23f93eb14570e2a23
Oliver Leung <oal22@cornell.edu>
2018-06-11 11:32:27 -0400

    Add command button sample code

--- main.py
+++ main.py
@@ -1,10 +1,80 @@
-import aide_design, aide_draw, aide_document, os, yaml
+import adsk.core, adsk.fusion, adsk.cam, traceback
 
-def main(folder_path):
-    with open(folder_path + '/params.yaml', 'w') as params_file:
-        params = yaml.load(folder_path + '/params.yaml')
-        # Process with aide_design
-        yaml.dump(params, params_file, default_flow_style=False)
+# Global list to keep all event handlers in scope.
+# This is only needed with Python.
+handlers = []
 
-def test():
-    print('AIDE has been run')
+def run(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+
+        # Get the CommandDefinitions collection.
+        cmdDefs = ui.commandDefinitions
+        
+        # Create a button command definition.
+        buttonSample = cmdDefs.addButtonDefinition('MyButtonDefIdPython', 
+                                                   'Python Sample Button', 
+                                                   'Sample button tooltip',
+                                                   './Resources/Sample')
+        
+        # Connect to the command created event.
+        sampleCommandCreated = SampleCommandCreatedEventHandler()
+        buttonSample.commandCreated.add(sampleCommandCreated)
+        handlers.append(sampleCommandCreated)
+        
+        # Get the ADD-INS panel in the model workspace. 
+        addInsPanel = ui.allToolbarPanels.itemById('SolidScriptsAddinsPanel')
+        
+        # Add the button to the bottom of the panel.
+        buttonControl = addInsPanel.controls.addCommand(buttonSample)
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
+
+# Event handler for the commandCreated event.
+class SampleCommandCreatedEventHandler(adsk.core.CommandCreatedEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args):
+        eventArgs = adsk.core.CommandCreatedEventArgs.cast(args)
+        cmd = eventArgs.command
+
+        # Connect to the execute event.
+        onExecute = SampleCommandExecuteHandler()
+        cmd.execute.add(onExecute)
+        handlers.append(onExecute)
+
+
+# Event handler for the execute event.
+class SampleCommandExecuteHandler(adsk.core.CommandEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args):
+        eventArgs = adsk.core.CommandEventArgs.cast(args)
+
+        # Code to react to the event.
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        ui.messageBox('In command execute event handler.')
+
+
+def stop(context):
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        
+        # Clean up the UI.
+        cmdDef = ui.commandDefinitions.itemById('MyButtonDefIdPython')
+        if cmdDef:
+            cmdDef.deleteMe()
+            
+        addinsPanel = ui.allToolbarPanels.itemById('SolidScriptsAddinsPanel')
+        cntrl = addinsPanel.controls.itemById('MyButtonDefIdPython')
+        if cntrl:
+            cntrl.deleteMe()
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
