13653ef78d461d101f48db7478f63bb96d1e6521
EN-CE-AC <EN-CE-AC@cornell.edu>
2018-06-11 11:56:15 -0400

    Add call to palette_gui

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import adsk.core, adsk.fusion, adsk.cam, traceback
+from aide_gui import palette_gui
 
 # Global list to keep all event handlers in scope.
 # This is only needed with Python.
@@ -12,21 +13,20 @@ def run(context):
 
         # Get the CommandDefinitions collection.
         cmdDefs = ui.commandDefinitions
-        
+
         # Create a button command definition.
-        buttonSample = cmdDefs.addButtonDefinition('MyButtonDefIdPython', 
-                                                   'Python Sample Button', 
-                                                   'Sample button tooltip',
-                                                   './Resources/Sample')
-        
+        buttonSample = cmdDefs.addButtonDefinition('AIDECommandDefIdPython',
+                                                   'Run AIDE',
+                                                   'Creates a design')
+
         # Connect to the command created event.
         sampleCommandCreated = SampleCommandCreatedEventHandler()
         buttonSample.commandCreated.add(sampleCommandCreated)
         handlers.append(sampleCommandCreated)
-        
-        # Get the ADD-INS panel in the model workspace. 
+
+        # Get the ADD-INS panel in the model workspace.
         addInsPanel = ui.allToolbarPanels.itemById('SolidScriptsAddinsPanel')
-        
+
         # Add the button to the bottom of the panel.
         buttonControl = addInsPanel.controls.addCommand(buttonSample)
     except:
@@ -58,19 +58,19 @@ class SampleCommandExecuteHandler(adsk.core.CommandEventHandler):
         # Code to react to the event.
         app = adsk.core.Application.get()
         ui  = app.userInterface
-        ui.messageBox('In command execute event handler.')
+        palette_gui.palette_gui.run(context)
 
 
 def stop(context):
     try:
         app = adsk.core.Application.get()
         ui  = app.userInterface
-        
+
         # Clean up the UI.
         cmdDef = ui.commandDefinitions.itemById('MyButtonDefIdPython')
         if cmdDef:
             cmdDef.deleteMe()
-            
+
         addinsPanel = ui.allToolbarPanels.itemById('SolidScriptsAddinsPanel')
         cntrl = addinsPanel.controls.itemById('MyButtonDefIdPython')
         if cntrl:
