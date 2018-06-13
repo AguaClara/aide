f77ed2ab022984a9637b33c7d2dcc82b8dfe1692
Oliver Leung <oal22@cornell.edu>
2018-06-12 23:30:34 -0400

    Change description

--- aide.py
+++ aide.py
@@ -1,13 +1,13 @@
 import sys, os, inspect, importlib
 import adsk.core, adsk.fusion, adsk.cam, traceback # Fusion 360 imports
-from .aide_gui import aide_gui
 
 # Takes a relative file path (String) to the calling file and returns the correct absolute path (String). Needed because the Fusion 360 environment doesn't resolve relative paths well.
 def abs_path(file_path):
     return os.path.join(os.path.dirname(inspect.getfile(sys._getframe(1))), file_path)
 
-# NOTE: This is probably not needed, but add it back if imports are broken and put aide packages after it.
-#sys.path.append(abs_path('.'))
+sys.path.append(abs_path('.'))
+
+from .aide_gui import aide_gui
 
 # Global list to keep all event handlers in scope.
 handlers = []
