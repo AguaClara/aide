9bd66385ccd67be9384d2baa6986c010f1ec132c
EN-CE-AC <EN-CE-AC@cornell.edu>
2018-06-25 12:48:11 -0400

    Implement aide_design

--- aide.py
+++ aide.py
@@ -1,6 +1,7 @@
 import sys, os, inspect, importlib
 import adsk.core, adsk.fusion, adsk.cam, traceback # Fusion 360 imports
 
+
 # Takes a relative file path (String) to the calling file and returns the correct absolute path (String). Needed because the Fusion 360 environment doesn't resolve relative paths well.
 def abs_path(file_path):
     return os.path.join(os.path.dirname(inspect.getfile(sys._getframe(1))), file_path)
@@ -8,6 +9,7 @@ def abs_path(file_path):
 sys.path.append(abs_path('.'))
 
 from .aide_gui import aide_gui, helper
+from .aide_draw import load_yaml_and_update_params
 
 # Global list to keep all event handlers in scope.
 handlers = []
@@ -38,4 +40,7 @@ def run_design():
     input_params = helper.load_yaml(abs_path("aide_gui/params.yaml"))
     lfom_params = helper.load_yaml(abs_path("lfom.yaml"))
     lfom_params['LFOM_1']['dp']['spacing'] = str(int(input_params['Flow Rate (L/s)']) * 2)
-    helper.write_yaml(abs_path('lfom.yaml'), lfom_params)
+    lfom_path = abs_path('lfom.yaml')
+    helper.write_yaml(lfom_path, lfom_params)
+    rootComponent = adsk.fusion.FusionDocument.cast(app.activeProduct.parentDocument).design.rootComponent
+    load_yaml_and_update_params(lfom_path, rootComponent)
